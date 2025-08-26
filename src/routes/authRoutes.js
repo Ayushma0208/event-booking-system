bd04ac79df36f312cf2f26b587694502be4d648c
Ayushma0208 <ayushmatripathi2002@gmail.com>
2025-08-27 01:06:07 +0530

    Initial project setup with structure and DB config

---
+++ src/routes/authRoutes.js
@@ -0,0 +1,11 @@
+import express from "express";
+// import { registerUser, loginUser, getProfile } from "../controllers/authController.js";
+
+const router = express.Router();
+
+// Routes
+// router.post("/register", registerUser);
+// router.post("/login", loginUser);
+// router.get("/profile", getProfile);
+
+export default router;
---
+++ src/server.js
@@ -0,0 +1,21 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+
+import authRoutes from "./routes/authRoutes.js";
+import eventRoutes from "./routes/eventRoutes.js";
+import bookingRoutes from "./routes/bookingRoutes.js";
+
+dotenv.config();
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+// Routes
+app.use("/api/auth", authRoutes);
+app.use("/api/events", eventRoutes);
+app.use("/api/bookings", bookingRoutes);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
