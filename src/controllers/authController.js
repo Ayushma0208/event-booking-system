2f52ac9878a1a51e46f64eeeb17e1aae11788d94
Ayushma0208 <ayushmatripathi2002@gmail.com>
2025-09-03 22:03:25 +0530

    delete profile

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -80,13 +80,10 @@ export const updateProfile = async (req, res) => {
 export const getUserProfile = async (req, res) => {
   try {
     const userId = req.user.id; 
-
     const user = await getUserById(userId);
-
     if (!user) {
       return res.status(404).json({ message: "User not found" });
     }
-
     res.json({message: "User profile fetched successfully",user});
   } catch (error) {
     console.error("Error fetching profile:", error);
@@ -104,10 +101,7 @@ export const deleteUser = async (req, res) => {
       return res.status(404).json({ message: "User not found" });
     }
 
-    res.status(200).json({
-      message: "User deleted successfully",
-      user: deletedUser
-    });
+    res.status(200).json({message: "User deleted successfully",user: deletedUser});
   } catch (error) {
     console.error("Controller error (deleteUser):", error);
     res.status(500).json({ message: "Server error" });
