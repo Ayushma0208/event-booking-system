8ebf1713828c9ac454ae3caeb939f624758e9b05
Ayushma0208 <ayushmatripathi2002@gmail.com>
2025-09-19 23:32:51 +0530

    event controller api

--- src/models/Events.js
+++ src/models/Events.js
@@ -1,7 +1,5 @@
 import pool from "../config/db.js";
 
-
-// Create Event
 export const createEvent = async ({ title, description, date, location, capacity, price }) => {
   const result = await pool.query(
     `INSERT INTO events (title, description, date, location, capacity, price)
@@ -12,13 +10,13 @@ export const createEvent = async ({ title, description, date, location, capacity
   return result.rows[0];
 };
 
-// Get All Events
+
 export const getAllEvents = async () => {
   const result = await pool.query(`SELECT * FROM events ORDER BY date ASC`);
   return result.rows;
 };
 
-// Get Single Event
+
 export const getEventById = async (id) => {
   const result = await pool.query(`SELECT * FROM events WHERE id = $1`, [id]);
   return result.rows[0];
