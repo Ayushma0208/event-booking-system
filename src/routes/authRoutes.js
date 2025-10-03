4edf5233ce96b6dd671c305702a2e00e7394ebd8
Ayushma0208 <ayushmatripathi2002@gmail.com>
2025-10-03 20:16:37 +0530

    removed unwanted space

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -3,17 +3,17 @@ import { changePasswordController, deleteUser, getUserProfile, loginUser, regist
 import { authMiddleware } from "../middleware/authMiddleware.js";
 const router = express.Router();
 
-router.post("/register", registerUser);
+router.post("/register", registerUser)
 
-router.post("/login", loginUser);
+router.post("/login", loginUser)
 
-router.put("/update-profile", authMiddleware,updateProfile);
+router.put("/update-profile", authMiddleware,updateProfile)
 
-router.get("/profile", authMiddleware, getUserProfile);
+router.get("/profile", authMiddleware, getUserProfile)
 
-router.delete("/delete", deleteUser);
+router.delete("/delete", deleteUser)
 
-router.patch("/change-password", authMiddleware, changePasswordController);
+router.patch("/change-password", authMiddleware, changePasswordController)
 
 
 export default router;
--- src/routes/bookingRoutes.js
+++ src/routes/bookingRoutes.js
@@ -3,18 +3,18 @@ import { assignSeat, cancelBookingController, checkBookingAvailabilityController
 
 const router = express.Router();
 
-router.post('/booking', createBookingController);
+router.post('/booking', createBookingController)
 
-router.get("/getAllBooking", getAllBookingsController);
+router.get("/getAllBooking", getAllBookingsController)
 
-router.get("/getSinglebookingId", getBookingByIdController);
+router.get("/getSinglebookingId", getBookingByIdController)
 
-router.put("/updateBookingId", updateBookingController);
+router.put("/updateBookingId", updateBookingController)
 
-router.delete("/cancelBookingId", cancelBookingController);
+router.delete("/cancelBookingId", cancelBookingController)
 
-router.get("/availability", checkBookingAvailabilityController);
+router.get("/availability", checkBookingAvailabilityController)
 
-router.post("/bookings/:id/assign-seat", assignSeat);
+router.post("/bookings/:id/assign-seat", assignSeat)
 
 export default router;
